321159fe63766606034d95739fd71407bd69c47f
githubhiseka <susantoabhiseka@gmail.com>
2024-12-10 00:36:24 +0700

    Update green.js

--- routes/green.js
+++ routes/green.js
@@ -63,70 +63,103 @@ router.get('/subkategori-detail/:subkategoriid', async (req, res) => {
                 FROM 
                     SUBKATEGORI_JASA sk
                     LEFT JOIN KATEGORI_JASA kj ON sk.KategoriJasaId = kj.Id
-                )
-                SELECT 
-                    sb.NamaSubkategori,
-                    sb.NamaKategori,
-                    sb.deskripsi,
-                    -- Pekerja info
-                    (
-                        SELECT json_agg(json_build_object(
-                            'nama_pekerja', nama
-                        ))
-                        FROM (
-                            SELECT DISTINCT p2.Nama as nama
-                            FROM "user" p2
-                            JOIN PEKERJA pk2 ON pk2.Id = p2.Id
-                            JOIN PEKERJA_KATEGORI_JASA pkj2 ON pkj2.PekerjaId = pk2.Id
-                            WHERE pkj2.KategoriJasaId = sb.kj_id
-                        ) as distinct_pekerja
-                    ) as pekerja_info,
-                    -- Sesi info
-                    (
-                        SELECT json_agg(json_build_object(
-                            'sesi', sesi,
-                            'harga', harga
-                        ))
-                        FROM (
-                            SELECT DISTINCT sl2.Sesi as sesi, sl2.Harga as harga
-                            FROM SESI_LAYANAN sl2
-                            WHERE sl2.SubkategoriId = sb.sk_id
-                            ORDER BY sl2.Sesi
-                        ) as distinct_sesi
-                    ) as sesi_info,
-                    -- Testimoni info
-                    (
-                        SELECT json_agg(json_build_object(
-                            'teks', teks,
-                            'rating', rating,
-                            'tanggal', tanggal
-                        ))
-                        FROM (
-                            SELECT DISTINCT t2.Teks as teks, t2.Rating as rating, t2.Tgl as tanggal
-                            FROM TESTIMONI t2
-                            JOIN TR_PEMESANAN_JASA tpj2 ON t2.IdTrPemesanan = tpj2.Id
-                            WHERE tpj2.IdKategoriJasa = sb.sk_id
-                        ) as distinct_testimoni
-                    ) as testimoni_info,
-                    (
-                        SELECT ROUND(AVG(t2.Rating)::numeric, 2)
+                WHERE sk.Id = $1
+            )
+            SELECT 
+                sb.NamaSubkategori,
+                sb.NamaKategori,
+                sb.deskripsi,
+                (
+                    SELECT json_agg(json_build_object(
+                        'pekerja_id', id,
+                        'nama_pekerja', nama
+                    ))
+                    FROM (
+                        SELECT p2.Id as id, p2.Nama as nama
+                        FROM "user" p2
+                        JOIN PEKERJA pk2 ON pk2.Id = p2.Id
+                        JOIN PEKERJA_KATEGORI_JASA pkj2 ON pkj2.PekerjaId = pk2.Id
+                        WHERE pkj2.KategoriJasaId = sb.kj_id
+                    ) as distinct_pekerja
+                ) as pekerja_info,
+                (
+                    SELECT json_agg(json_build_object(
+                        'sesi', sesi,
+                        'harga', harga
+                    ))
+                    FROM (
+                        SELECT DISTINCT sl2.Sesi as sesi, sl2.Harga as harga
+                        FROM SESI_LAYANAN sl2
+                        WHERE sl2.SubkategoriId = sb.sk_id
+                        ORDER BY sl2.Sesi
+                    ) as distinct_sesi
+                ) as sesi_info,
+                (
+                    SELECT json_agg(json_build_object(
+                        'teks', teks,
+                        'rating', rating,
+                        'tanggal', tanggal
+                    ))
+                    FROM (
+                        SELECT DISTINCT t2.Teks as teks, t2.Rating as rating, t2.Tgl as tanggal
                         FROM TESTIMONI t2
                         JOIN TR_PEMESANAN_JASA tpj2 ON t2.IdTrPemesanan = tpj2.Id
                         WHERE tpj2.IdKategoriJasa = sb.sk_id
-                    ) as avg_rating
-                FROM 
-                    SubkategoriBase sb
-                WHERE
-                    sb.sk_id = $1
-                ORDER BY 
-                    sb.NamaKategori,
-                    sb.NamaSubkategori;`,
+                    ) as distinct_testimoni
+                ) as testimoni_info,
+                (
+                    SELECT ROUND(AVG(t2.Rating)::numeric, 2)
+                    FROM TESTIMONI t2
+                    JOIN TR_PEMESANAN_JASA tpj2 ON t2.IdTrPemesanan = tpj2.Id
+                    WHERE tpj2.IdKategoriJasa = sb.sk_id
+                ) as avg_rating
+            FROM 
+                SubkategoriBase sb`,
             [subkategoriId]
         );
         res.json(results.rows);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error fetching data');
+    }
+});
+
+router.get('/get-metode-bayar', async (req, res) => {
+    try {
+        const results = await client.query(
+            'SELECT * FROM metode_bayar' // Query yang akan dijalankan
+        );
+        res.json(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+router.get('/check-discount', async (req, res) => {
+    try {
+        const results = await client.query(
+            'SELECT * FROM diskon' // Query yang akan dijalankan
+        );
+        res.json(results.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+router.post('/insert-pemesanan-jasa', async (req, res) => {
+    const { tanggal, idPelanggan, idKategori, totalHarga, idMetodeBayar } = req.query;
+    try {
+        // Pertama, harus bikin diskon baru sebagai syarat voucher
+        await client.query(
+            'INSERT INTO tr_pemesanan_jasa (tglpemesanan, idpelanggan, idkategorijasa, totalbiaya, idmetodebayar) VALUES ($1, $2, $3, $4,$5)',
+            [tanggal, idPelanggan, idKategori, totalHarga, idMetodeBayar]
+        );
+        res.json("Data inserted successfully!");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error inserting data');
     }
 });
 
